ab0e090e7c796bbe8312793ebcf78882c8fb27e7
Guilermo <gcrb@cin.ufpe.br>
2019-06-26 22:17:04 -0300

    tutorial 3 primeiros ratos

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -18,17 +18,17 @@ onready var squeek = $Mouse/MouseTemp/Squeek
 onready var squaak = $Mouse/MouseTemp/Squaak
 
 onready var flute_song100 = $FluteSong/FluteSong100
-onready var flute_song120 = $FluteSong/FluteSong120
-onready var flute_song140 = $FluteSong/FluteSong140
-onready var flute_song200 = $FluteSong/FluteSong200
-onready var C = $FluteSong/C
-onready var D = $FluteSong/D
-onready var E = $FluteSong/E
-onready var F = $FluteSong/F
-onready var G = $FluteSong/G
-onready var A = $FluteSong/A
-onready var B = $FluteSong/B
-onready var C8 = $FluteSong/C8
+onready var note_1 = $FluteSong/note_1
+onready var note_2 = $FluteSong/note_2
+
+onready var S = $Keys/S
+onready var K = $Keys/K
+
+onready var S_text = preload("res://SPRITES/TECLADO/S.png")
+onready var S_pressed_text = preload("res://SPRITES/TECLADO/S_pressed.png")
+onready var K_text = preload("res://SPRITES/TECLADO/K.png")
+onready var K_pressed_text = preload("res://SPRITES/TECLADO/K_pressed.png")
+
 
 onready var acerto0 = false
 onready var acerto1 = false
@@ -45,68 +45,110 @@ func _process(delta):
 	$Mouse.mouseMovement(speed, score)
 	$FluteSong.playFluteSong(score)
 	
-	#Compartamento dos ratos
-	if time == 60:
+	#tutorial################
+	if time == 30:
+		S.show()
+	if time == 55:
 		MouseTemp.show()
 		squeek.play()
-	if time >= 85 && time <= 95:
+	if time == 90:
+		S.texture = S_pressed_text
+		note_1.play()
+		MouseTemp.hide()
+	if time == 110:
+		S.texture = S_text
+	if time == 115: 
+		S.hide()
+	
+	
+	if time == 150:
+		K.show()
+	if time == 180:
+		MouseTemp.show()
+		squaak.play()
+	if time == 210:
+		K.texture = K_pressed_text
+		note_2.play()
+		MouseTemp.hide()
+	if time == 225: 
+		K.texture = K_text
+	if time == 245:
+		K.hide()
+	############################
+	
+	#1o rato
+	if time == 535:
+		MouseTemp.show()
+		squeek.play()
+	if time >= 565 && time <= 575:
 		if Input.is_action_pressed("button_s"):
-			if !C.playing:
-				C.play()
+			if !note_1.playing:
+				note_1.play()
 				MouseTemp.hide()
 				Mouse0.show()
-				score += 1
-	if time == 95: 
+	if time == 575:
 		MouseTemp.hide()
-		
-	if time == 175:
+	
+	#2o rato
+	if time == 655:
 		MouseTemp.show()
 		squaak.play()
-	if time >= 200 && time <=210:
+	if time >= 680 && time <= 690:
 		if Input.is_action_pressed("button_k"):
-			if !A.playing:
-				A.play()
+			if !note_2.playing:
+				note_2.play()
 				MouseTemp.hide()
 				Mouse1.show()
-				score += 1
-	if time == 210: 
+	if time == 685:
 		MouseTemp.hide()
-		
-	if time == 360:
+	
+	#tutorial ######################
+	if time == 985:
+		S.show()
+		K.show()
+	if time == 1015:
 		MouseTemp.show()
 		squeek.play()
-	if time == 390:
+	if time == 1045:
 		squaak.play()
 	
-#	if time == 440:
-#		squeek.play()
-#	if time == 455:
-#		squaak.play()
-		
-	if time >= 435 && time <= 445:
-		if Input.is_action_pressed("button_s"):
-			if !E.playing:
-				E.play()
-				acerto0 = true
-	if time >= 445 && time <= 460:
-		if Input.is_action_pressed("button_k"):
-			if !A.playing:
-				E.stop()
-				A.play()
-				acerto1 = true
-				if acerto0 && acerto1:
-					MouseTemp.hide()
-					Mouse2.show()
-					score += 1
-	if time == 460: 
+	if time == 1160:
 		MouseTemp.hide()
-		acerto0 = false
-		acerto1 = false
+		S.texture = S_pressed_text
+		note_1.play()
+	if time == 1170:
+		S.texture = S_text
+		K.texture = K_pressed_text
+		note_2.play() 
+	if time == 1190:
+		S.hide()
+		K.hide()
+	###############################
+	
+	#3o rato
+	if time == 1500:
+		MouseTemp.show()
+		squeek.play()
+	if time == 1530:
+		squaak.play()
+	
+	if time >= 1630 && time <= 1640:
+		if Input.is_action_pressed("button_s"):
+			if !note_1.playing:
+				note_1.play()
+				acerto0 = true
+	if time >= 1640 && time <= 1655:
+		if Input.is_action_pressed("button_k"):
+				if !note_2.playing:
+					note_2.play()
+					note_1.stop()
+					MouseTemp.hide()
+				if acerto0:
+					Mouse2.show()
+	if time == 1455:
+		MouseTemp.hide()
 		
 	time += 1
-	
-	if score >= 3:
-		speed = 2.4
 	pass
 	
 	
