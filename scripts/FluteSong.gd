45f29037764ae0a393209180650350056826af27
Guilermo <gcrb@cin.ufpe.br>
2019-06-27 14:21:04 -0300

    excluindo arquivos desnecsessários

--- scripts/FluteSong.gd
+++ scripts/FluteSong.gd
@@ -2,29 +2,9 @@ extends Node2D
 
 
 onready var flute_song100 = $FluteSong100
-onready var flute_song120 = $FluteSong120
-onready var flute_song140 = $FluteSong140
-onready var flute_song200 = $FluteSong200
 
 func playFluteSong(score):
 	if score < 3:
 		if !flute_song100.playing:
-#			flute_song120.play()
-			flute_song140.play()
-			flute_song140.stop()
 			flute_song100.play()
-	elif score < 8:
-		if !flute_song120.playing:
-			flute_song100.stop()
-			flute_song140.stop()
-			flute_song120.play()
-	elif score < 12:
-		if !flute_song140.playing:
-			flute_song120.stop()
-			flute_song200.stop()
-			flute_song140.play()
-	else:
-		if !flute_song200.playing:
-			flute_song140.stop()
-			flute_song120.play()
-	pass
+
