2346e242b757ff481a9a2f3149c1d9a868776289
Guilermo <gcrb@cin.ufpe.br>
2019-06-29 09:22:21 -0300

    gerando loop

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -5,15 +5,23 @@ extends Node2D
 var speed = 2
 #pontuacao
 var score = 0
+#
 
 
 #ratos
+
 onready var MouseTemp = $Mouse/MouseTemp/MouseTemp
 onready var Mouse0 = $Mouse/Mice/Mouse0
 onready var Mouse1 = $Mouse/Mice/Mouse1
 onready var Mouse2 = $Mouse/Mice/Mouse2
 onready var Mouse3 = $Mouse/Mice/Mouse3
-
+onready var Mouse4 = $Mouse/Mice/Mouse4
+onready var Mouse5 = $Mouse/Mice/Mouse5
+onready var Mouse6 = $Mouse/Mice/Mouse6
+onready var Mouse7 = $Mouse/Mice/Mouse7
+onready var Mouse8 = $Mouse/Mice/Mouse8
+onready var Mouse9 = $Mouse/Mice/Mouse9
+#
 onready var squeek = $Mouse/MouseTemp/Squeek
 onready var squaak = $Mouse/MouseTemp/Squaak
 
@@ -80,9 +88,9 @@ func _process(delta):
 	
 	if firstTutOver:
 		if score <= 10:
-			randNum = randi()%2+0 #numero randomico 0 ou 1 
 			var elapsedMod = elapsed%2000
-			if elapsedMod >= 1000 && elapsedMod <= 1100:
+			if elapsedMod >= 1000 && elapsedMod <= 1100 && !randLock:
+				randNum = randi()%2+0 #numero randomico 0 ou 1 
 				MouseTemp.show()
 				if randNum == 0:
 					if !squeek.playing:
@@ -90,6 +98,7 @@ func _process(delta):
 				if randNum == 1:
 					if !squaak.playing:
 						squaak.play()
+				randLock = true
 			if elapsedMod >= 1400 && elapsedMod <= 1700:
 				if randNum == 0:
 					if Input.is_action_pressed("button_s"):
@@ -97,94 +106,16 @@ func _process(delta):
 							E.play()
 							MouseTemp.hide()
 							Mouse0.show()
-				if randNum == 1:
+				elif randNum == 1:
 					if Input.is_action_pressed("button_k"):
 						if !A.playing:
 							A.play()
 							MouseTemp.hide()
 							Mouse0.show()
-			if elapsedMod  >= 1600 && elapsedMod <= 1700:
+			if elapsedMod  >= 1700 && elapsedMod <= 1800:
 				MouseTemp.hide()
-					
-					
+				randLock = false
 				
+					
+			
 	
-	
-#	############################
-#	#1o rato
-#	if time == 535:
-#		MouseTemp.show()
-#		squeek.play()
-#	if time >= 565 && time <= 575:
-#		if Input.is_action_pressed("button_s"):
-#			if !E.playing:
-#				E.play()
-#				MouseTemp.hide()
-#				Mouse0.show()
-#	if time == 575:
-#		MouseTemp.hide()
-#
-#	#2o rato
-#	if time == 655:
-#		MouseTemp.show()
-#		squaak.play()
-#	if time >= 680 && time <= 690:
-#		if Input.is_action_pressed("button_k"):
-#			if !A.playing:
-#				A.play()
-#				MouseTemp.hide()
-#				Mouse1.show()
-#	if time == 685:
-#		MouseTemp.hide()
-#
-#	#tutorial ######################
-#	if time == 985:
-#		S.show()
-#		K.show()
-#	if time == 1015:
-#		MouseTemp.show()
-#		squaak.play()
-#	if time == 1045:
-#		squeek.play()
-#
-#	if time == 1160:
-#		MouseTemp.hide()
-#		K.texture = K_pressed_text
-#		Flow.play()
-#	if time == 1170:
-#		K.texture = K_text
-#		S.texture = S_pressed_text
-#		Flow.stop()
-#		Elow.play() 
-#	if time == 1190:
-#		S.hide()
-#		K.hide()
-#		S.texture = S_text
-#	###############################
-#
-#	#3o rato
-#	if time == 1500:
-#		MouseTemp.show()
-#		squaak.play()
-#	if time == 1530:
-#		squeek.play()
-#
-#	if time >= 1620 && time <= 1640:
-#		if Input.is_action_pressed("button_k"):
-#			if !Flow.playing:
-#				Flow.play()
-#				acerto0 = true
-#	if time >= 1640 && time <= 1655:
-#		if Input.is_action_pressed("button_s"):
-#				if !Elow.playing:
-#					Elow.play()
-#					Flow.stop()
-#					MouseTemp.hide()
-#				if acerto0:
-#					Mouse2.show()
-#	if time == 1655:
-#		MouseTemp.hide()
-#
-		
-		
-		
